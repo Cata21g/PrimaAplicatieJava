3cffbb932cd7f0b0db2a35d35ca8268417d222b4
gogu <gogucatalin@gmail.com>
2021-02-12 22:15:43 +0200

    upload Prima Apliatie Java

---
+++ src/com/company/ctrlN/Main.java
@@ -0,0 +1,9 @@
+package com.company.ctrlN;
+
+public class Main {
+
+    public static void main(String[]  args) {
+            System.out.println("Invat limbajul Java si despre masina virtuala java!");
+
+    }
+}
