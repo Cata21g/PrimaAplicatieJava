146864158d67d43ec72f69149a9a7e26a22be480
gogu <gogucatalin@gmail.com>
2021-02-13 20:44:04 +0200

    adaugare comentari pe o singura linie

--- src/com/company/ctrlN/Main.java
+++ src/com/company/ctrlN/Main.java
@@ -1,8 +1,10 @@
 package com.company.ctrlN;
 
 public class Main {
-
+    // Aceasta este metoda main
     public static void main(String[]  args) {
+        //Aici vrem sa afisam pe consola
+        // intelij mesajul de mai jos
             System.out.println("Invat limbajul Java si despre masina virtuala java!");
 
     }
