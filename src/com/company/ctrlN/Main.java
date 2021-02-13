6e9acf4d7f11adaca739b88ff0736869b74e9ab3
gogu <gogucatalin@gmail.com>
2021-02-13 20:58:23 +0200

    adaugare multi-comm

--- src/com/company/ctrlN/Main.java
+++ src/com/company/ctrlN/Main.java
@@ -2,9 +2,14 @@ package com.company.ctrlN;
 
 public class Main {
     // Aceasta este metoda main
-    public static void main(String[]  args) {
-        //Aici vrem sa afisam pe consola
+    /*
+    comm
+     */
+    public static void main(/*args este parametrul metodei main*/String[]  args) {
+        /*Aici vrem sa afisam pe consola
         // intelij mesajul de mai jos
+        Textul de aici face parte tot din commentariu
+        */
             System.out.println("Invat limbajul Java si despre masina virtuala java!");
 
     }
