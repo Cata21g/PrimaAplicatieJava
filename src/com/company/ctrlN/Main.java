414954c0dde0136dc8a0fa340b97cd8bcc1b9ab4
Cata21g <76911226+Cata21g@users.noreply.github.com>
2021-02-14 19:25:19 +0200

    da functioneaza

--- src/com/company/ctrlN/Main.java
+++ src/com/company/ctrlN/Main.java
@@ -5,9 +5,11 @@ public class Main {
      * Metoda main este metoda apelata de catre masina virtuala java si care porneste o aplicatie java.
      * @param args Acesta este un parametru.
      * @author Catalin
+     * da functioneaza
      */
     public static void main(String[]  args) {
-            System.out.println("Invat limbajul Java si despre masina virtuala java!");
 
+            System.out.println("Invat limbajul Java si despre masina virtuala java!");
+            com.company.misc.Main.main(args);
     }
 }
---
+++ src/com/company/misc/Main.java
@@ -0,0 +1,7 @@
+package com.company.misc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Salutare din com.company.misc.Main");
+    }
+}
